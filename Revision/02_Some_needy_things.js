5307d6637944e4570a804eba7d74483f7e9d385f
Gundarahull <r161731@rguktrkv.ac.in>
2024-09-19 16:48:48 +0530

    testing

--- Revision/02_Some_needy_things.js
+++ Revision/02_Some_needy_things.js
@@ -1,8 +1,8 @@
 //PROJECTION--- what i want to show
 
-db.users.findOne({name:"Rahul Sharma"},{age:1,branch:1})
+db.users.findOne({ name: "Rahul Sharma" }, { age: 1, branch: 1 });
 //1 - means have to show,0- means no need to show
-ulti> db.users.findOne({name:"Rahul Sharma"},{age:1,branch:1})
+ulti > db.users.findOne({ name: "Rahul Sharma" }, { age: 1, branch: 1 });
 // {
 //   _id: ObjectId('66cd77783b9f145428228fb8'),
 //   age: 25,
@@ -14,7 +14,7 @@ ulti> db.users.findOne({name:"Rahul Sharma"},{age:1,branch:1})
 //Number,String,Array,Object,Date,Boolean
 
 //to find what type od dataStructure its
-typeof db.users.findOne().age
+typeof db.users.findOne().age;
 
 //see here in the entercollection
 // ulti> typeof db.users.findOne().favoriteMarvelHeroes
@@ -28,18 +28,17 @@ typeof db.users.findOne().age
 // ulti> typeof user.favoriteMarvelHeroes; // Should return 'object'
 // object
 
-
 //DROP DATABASE
-db.users.drop()
+db.users.drop();
 
 //writeConcern
 //we normally use these in WRITE functions.....
 //like INSERT and UPDATE
 db.users.insertOne(
-    {name: "Rahul Sharma", age: 25},
-    {
-        writeConcern: { w: 0, j: true, wtimeout: 1000 }
-    }
+  { name: "Rahul Sharma", age: 25 },
+  {
+    writeConcern: { w: 0, j: true, wtimeout: 1000 },
+  }
 );
 // writeConcern is an object that contains w, j, and wtimeout:
 // w: 0 means no write acknowledgment is required.                      w--correct inserT or update ayindho ledho kuda guantee lw when we insert 0 1-aithe pakka ayindhi andi
@@ -50,3 +49,9 @@ db.users.insertOne(
 //aithe andahru lopalaki pondi,
 //yemama issue jarigindhi,  andharu baytiki vachi dengandi anthay...
 //example
+db.users.insertOne(
+  { name: "Rahul Sharma", age: 25 },
+  {
+    writeConcern: { w: 1, j: true },
+  }
+);
